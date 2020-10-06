ed056266cf1c36f587bf886eb3c1cd9032da1efc
ItsTheFlea <59777514+ItsTheFlea@users.noreply.github.com>
2020-10-06 18:20:06 -0400

    Initial Upload

---
+++ BasicIO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GetUserData();
+            Console.ReadLine();
+        }
+        private static void GetUserData()
+        {
+            System.Console.Write("Your Name: ");
+            string userName = Console.ReadLine();
+            System.Console.Write("Your Age: ");
+            string userAge = Console.ReadLine();
+
+            System.Console.WriteLine("Hello {0}! You are {1} years old!", userName, userAge);
+        }
+
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("***********************************");
+            Console.WriteLine("*****Welcome to My Rocking App*****");
+            Console.WriteLine("***********************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.ReadLine();
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+    }
+}
