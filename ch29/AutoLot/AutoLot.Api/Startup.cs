1c7fa9d5adf8c5dddaa9a62e3fb72f1e361707a5
ItsTheFlea <59777514+ItsTheFlea@users.noreply.github.com>
2020-12-15 22:51:42 -0500

    Classwork
    Working on creating an api for our autolot database

---
+++ ch29/AutoLot/AutoLot.Api/Startup.cs
@@ -0,0 +1,113 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using AutoLot.Dal.EfStructures;
+using AutoLot.Dal.Initialization;
+using AutoLot.Dal.Repos;
+using AutoLot.Dal.Repos.Interfaces;
+
+namespace AutoLot.Api
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _env = env;
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .ConfigureApiBehaviorOptions(options => 
+                {
+                    options.SuppressInferBindingSourcesForParameters = true;
+                    options.SuppressModelStateInvalidFilter = true;
+                })
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                    options.JsonSerializerOptions.WriteIndented = true;
+                });
+
+            var connectionString = Configuration.GetConnectionString("AutoLot");
+
+            services.AddDbContextPool<ApplicationDbContext>(
+                options => options.UseSqlServer(connectionString,
+                sqlOptions => sqlOptions.EnableRetryOnFailure()));
+
+            services.AddScoped<ICarRepo, CarRepo>();
+            services.AddScoped<ICreditRiskRepo, CreditRiskRepo>();
+            services.AddScoped<ICustomerRepo, CustomerRepo>();
+            services.AddScoped<IMakeRepo, MakeRepo>();
+            services.AddScoped<IOrderRepo, OrderRepo>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "AutoLot Service",
+                        Version = "v1",
+                        Description = "Service to support the AutoLot dealer site",
+                        License = new OpenApiLicense
+                        {
+                            Name = "Apress Publishing",
+                            Url = new Uri("https://www.Apress.com")
+                        }
+                    });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            if (Configuration.GetValue<bool>("RebuildDatabase"))
+            {
+                SampleDataInitializer.InitializeData(context);
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AutoLot Service v1");
+            });
+        }
+    }
+}
